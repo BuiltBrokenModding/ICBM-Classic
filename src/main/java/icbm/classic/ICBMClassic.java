111e8b6c3aec7e872a3307c272b829b3fd501788
Robert Seifert <rseifert.phone@gmail.com>
2017-04-06 15:04:27 -0400

    Changed to VE ID

--- src/main/java/icbm/classic/ICBMClassic.java
+++ src/main/java/icbm/classic/ICBMClassic.java
@@ -83,7 +83,7 @@ public final class ICBMClassic extends AbstractMod
     public static final String REVISION_VERSION = "@REVIS@";
     public static final String BUILD_VERSION = "@BUILD@";
     public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION + "." + BUILD_VERSION;
-    public static final String DEPENDENCIES = "required-after:VoltzEngine;after:OpenComputers";
+    public static final String DEPENDENCIES = "required-after:voltzengine;after:OpenComputers";
 
     public static final int ENTITY_ID_PREFIX = 50;
 
