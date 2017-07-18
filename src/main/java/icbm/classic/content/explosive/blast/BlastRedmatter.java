806aca370bd30f05aa3a91b7f208a7be4ac94ea0
Robert Seifert <rseifert.phone@gmail.com>
2017-07-17 20:15:53 -0400

    Added custom damage type for redmatter
    This should bypass armor but will need more work down the road.

--- src/main/java/icbm/classic/content/explosive/blast/BlastRedmatter.java
+++ src/main/java/icbm/classic/content/explosive/blast/BlastRedmatter.java
@@ -265,7 +265,7 @@ public class BlastRedmatter extends Blast
             }
             else if(entity instanceof EntityLiving)
             {
-                ((EntityLiving)entity).attackEntityFrom(DamageSource.outOfWorld, 2000);
+                entity.attackEntityFrom(new DamageSourceRedmatter(this), 2000);
             }
             else
             {
@@ -275,6 +275,17 @@ public class BlastRedmatter extends Blast
         }
 
         return explosionCreated;
+    }
+
+    public static class DamageSourceRedmatter extends DamageSource
+    {
+        public final BlastRedmatter blastRedmatter;
+
+        public DamageSourceRedmatter(BlastRedmatter blastRedmatter)
+        {
+            super("icbm.redmatter");
+            this.blastRedmatter = blastRedmatter;
+        }
     }
 
     /**
