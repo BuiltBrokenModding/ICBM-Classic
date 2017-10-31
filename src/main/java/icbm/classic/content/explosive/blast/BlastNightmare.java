992bd31fd7fcc5c71b6879e68d391a156aa40608
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-31 15:54:40 -0400

    Reduced spawn count for Nightmare missile, Implemented explosive bats

--- src/main/java/icbm/classic/content/explosive/blast/BlastNightmare.java
+++ src/main/java/icbm/classic/content/explosive/blast/BlastNightmare.java
@@ -1,12 +1,14 @@
 package icbm.classic.content.explosive.blast;
 
+import com.builtbroken.mc.core.content.entity.bat.ex.EntityExBat;
 import com.builtbroken.mc.framework.thread.delay.DelayedAction;
 import com.builtbroken.mc.framework.thread.delay.DelayedActionHandler;
 import com.builtbroken.mc.imp.transform.vector.Pos;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.world.World;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -22,34 +24,38 @@ public class BlastNightmare extends Blast
     @Override
     protected void doExplode()
     {
-        final Pos center = new Pos(this);
-        final int size = (int) this.explosionSize;
-
-        //Spawn bats
-        final int batCount = (size / 2) + oldWorld().rand.nextInt(size);
-        for (int i = 0; i < batCount; i++)
+        if(!oldWorld().isRemote)
         {
-            //TODO make new bat mob that can attack players and has a pumpkin for a head, have it shoot flames out behind it as well
-            //TODO distribute using missile miss spread code
-            EntityBat bat = new EntityBat(oldWorld());
-            DelayedSpawn delay = new DelayedSpawn(oldWorld(), center, bat, 10, (i + oldWorld().rand.nextInt(size)) * 20); //reduce bats spawning at same time
-            DelayedActionHandler.add(delay);
-        }
+            final Pos center = new Pos(this);
+            final int size = (int) this.explosionSize;
 
-        //Spawn monsters
-        final int monsterCount = (size / 2) + oldWorld().rand.nextInt(size);
-        for (int i = 0; i < monsterCount; i++)
-        {
-            //TODO distribute using missile miss spread code
-            //TODO materialize zombies as ghosts so they can walk through walls in order to find an air pocket to spawn
-            EntityZombie zombie = new EntityZombie(oldWorld());
-            DelayedSpawn delay = new DelayedSpawn(oldWorld(), center, zombie, 10, (i + oldWorld().rand.nextInt(size * 2)) * 20); //reduce bats spawning at same time
-            DelayedActionHandler.add(delay);
-        }
+            //TODO cache delays created by this blast to allow for /lag command to clear
 
-        //TODO play deathly scream
-        //TODO replace torches with bone torch set for random halloween colors and low light levels
-        //TODO have monsters move towards players
+            //Spawn bats
+            final int batCount = (size / 10) + oldWorld().rand.nextInt(size / 10);
+            for (int i = 0; i < batCount; i++)
+            {
+                EntityExBat bat = new EntityExBat(oldWorld());
+                bat.ai_type = EntityExBat.TYPE_HOSTILE;
+                bat.deathTime = (int) TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES) * 20;
+                bat.setExplosive(null, 2, null); //Set to TNT for the moment
+                DelayedActionHandler.add(new DelayedSpawn(oldWorld(), center, bat, 10, (i + oldWorld().rand.nextInt(size)) * 20));
+            }
+
+            //Spawn monsters
+            final int monsterCount = (size / 10) + oldWorld().rand.nextInt(size / 10);
+            for (int i = 0; i < monsterCount; i++)
+            {
+                //TODO distribute using missile miss spread code
+                //TODO materialize zombies as ghosts so they can walk through walls in order to find an air pocket to spawn
+                EntityZombie zombie = new EntityZombie(oldWorld());
+                DelayedActionHandler.add(new DelayedSpawn(oldWorld(), center, zombie, 10, (i + oldWorld().rand.nextInt(size * 2)) * 20));
+            }
+
+            //TODO play deathly scream
+            //TODO replace torches with bone torch set for random halloween colors and low light levels
+            //TODO have monsters move towards players
+        }
     }
 
     public static class DelayedSpawn extends DelayedAction
