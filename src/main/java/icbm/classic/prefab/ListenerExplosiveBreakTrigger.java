5ba85942e70e0ec50a542d5ca662fa8b1b90d3dd
Robert WC Seifert <rseifert.phone@gmail.com>
2017-12-18 07:34:49 -0500

    More work on redmatter ore

--- src/main/java/icbm/classic/prefab/ListenerExplosiveBreakTrigger.java
+++ src/main/java/icbm/classic/prefab/ListenerExplosiveBreakTrigger.java
@@ -2,9 +2,11 @@ package icbm.classic.prefab;
 
 import com.builtbroken.mc.framework.block.imp.*;
 import com.builtbroken.mc.framework.json.loading.JsonProcessorData;
+import com.builtbroken.mc.lib.helper.MathUtility;
 import com.builtbroken.mc.seven.framework.block.listeners.TileListener;
 import icbm.classic.content.explosive.blast.BlastRedmatter;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.Explosion;
@@ -23,11 +25,29 @@ public class ListenerExplosiveBreakTrigger extends TileListener implements IDest
     @JsonProcessorData(value = "size", type = "float")
     public float size = 1;
 
+    @JsonProcessorData(value = "rightClickTriggerChance", type = "float")
+    public float rightClickTriggerChance = 0;
+
+    @JsonProcessorData(value = "leftClickTriggerChance", type = "float")
+    public float leftClickTriggerChance = 0;
+
     @JsonProcessorData(value = {"explosiveID", "ex"})
     public String explosiveID;
 
     @JsonProcessorData(value = "timer", type = "int")
     public int timer = 1000;
+
+    @JsonProcessorData(value = "triggerOnSilkTouch")
+    public boolean triggerOnSilkTouch = false;
+
+    public final Block block;
+
+    private boolean handled = false;
+
+    public ListenerExplosiveBreakTrigger(Block block)
+    {
+        this.block = block;
+    }
 
     @Override
     public void onDestroyedByExplosion(Explosion ex)
@@ -38,21 +58,36 @@ public class ListenerExplosiveBreakTrigger extends TileListener implements IDest
     @Override
     public void breakBlock(Block block, int meta)
     {
-        trigger(null);
-        //trigger(null); TODO check if this triggers even on silk harvest
+        //Trigger for automation
+        if (!handled && getBlock() == block)
+        {
+            trigger(null);
+        }
+        //Reset
+        handled = false;
     }
 
     @Override
     public boolean removedByPlayer(EntityPlayer player, boolean willHarvest)
     {
-        trigger(player);
-        return world().unwrap().setBlockToAir(xi(), yi(), zi());
+        //Note we have already handled the event
+        handled = true;
+
+        //Check for silk touch to avoid triggering
+        if (!triggerOnSilkTouch || !block.canSilkHarvest(world().unwrap(), player, xi(), yi(), zi(), getBlockMeta()) || EnchantmentHelper.getSilkTouchModifier(player))
+        {
+            trigger(player);
+            //Remove block
+            return world().unwrap().setBlockToAir(xi(), yi(), zi());
+        }
+        return false;
     }
 
     protected void trigger(Entity entity)
     {
-        if(isServer() && explosiveID != null)
+        if (isServer() && explosiveID != null)
         {
+            //TODO add other types
             if (explosiveID.equalsIgnoreCase("redmatter"))
             {
                 BlastRedmatter blast = new BlastRedmatter(world().unwrap(), entity, x() + 0.5, y() + 0.5, z() + 0.5, size);
@@ -62,23 +97,26 @@ public class ListenerExplosiveBreakTrigger extends TileListener implements IDest
         }
     }
 
-
-    @Override
-    public boolean canHarvest(EntityPlayer player, int meta)
-    {
-        return false; //TODO setup so can only silk harvest
-    }
-
     @Override
     public boolean onPlayerActivated(EntityPlayer player, int side, float hitX, float hitY, float hitZ)
     {
-        return false; //TODO chance to explode
+        if (MathUtility.rand.nextFloat() < rightClickTriggerChance) //TODO check to make sure mining doesn't trigger with silk touch
+        {
+            trigger(player);
+            world().unwrap().setBlockToAir(xi(), yi(), zi());
+        }
+        return false;
     }
 
     @Override
     public boolean onPlayerClicked(EntityPlayer player)
     {
-        return false; //TODO chance to explode
+        if (MathUtility.rand.nextFloat() < leftClickTriggerChance)
+        {
+            trigger(player);
+            world().unwrap().setBlockToAir(xi(), yi(), zi());
+        }
+        return false;
     }
 
     @Override
@@ -95,7 +133,7 @@ public class ListenerExplosiveBreakTrigger extends TileListener implements IDest
         @Override
         public ITileEventListener createListener(Block block)
         {
-            return new ListenerExplosiveBreakTrigger();
+            return new ListenerExplosiveBreakTrigger(block);
         }
 
         @Override
