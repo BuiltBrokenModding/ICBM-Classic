1a1cca320ef93fe26a8345e020073a614625122e
Robert WC Seifert <rseifert.phone@gmail.com>
2018-01-01 02:31:17 -0500

    Moved delay spawn object to VE

--- src/main/java/icbm/classic/content/explosive/blast/BlastNightmare.java
+++ src/main/java/icbm/classic/content/explosive/blast/BlastNightmare.java
@@ -1,8 +1,8 @@
 package icbm.classic.content.explosive.blast;
 
 import com.builtbroken.mc.core.content.entity.bat.ex.EntityExBat;
-import com.builtbroken.mc.framework.thread.delay.DelayedAction;
 import com.builtbroken.mc.framework.thread.delay.DelayedActionHandler;
+import com.builtbroken.mc.framework.thread.delay.DelayedSpawn;
 import com.builtbroken.mc.imp.transform.vector.Pos;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityZombie;
@@ -55,75 +55,6 @@ public class BlastNightmare extends Blast
             //TODO play deathly scream
             //TODO replace torches with bone torch set for random halloween colors and low light levels
             //TODO have monsters move towards players
-        }
-    }
-
-    public static class DelayedSpawn extends DelayedAction
-    {
-        Pos center;
-        Entity entity;
-        int range;
-
-        int spawnAttemptLimit = 10;
-        private int spawnAttempts;
-
-        public DelayedSpawn(World world, Pos center, Entity entity, int range, int ticks)
-        {
-            super(world, ticks);
-            this.center = center;
-            this.entity = entity;
-            this.range = range;
-        }
-
-        @Override
-        public boolean trigger()
-        {
-            if (entity != null)
-            {
-                final float width = entity.width;
-                final float height = entity.height;
-
-                //Code can only handle entities roughly 1 block wide
-                if (width <= 1.1)
-                {
-                    spawnAttempts++;
-
-                    //Get random spawn point
-                    Pos spawnPoint = center.addRandom(world.rand, range);
-                    if (!spawnPoint.isAirBlock(world))
-                    {
-                        spawnPoint = spawnPoint.add(0, 1, 0);
-                    }
-
-                    //Ensure point is in air and is on ground
-                    if (spawnPoint.isAirBlock(world) && !spawnPoint.sub(0, 1, 0).isAirBlock(world))
-                    {
-                        //Do height check
-                        if (height > 1)
-                        {
-                            int heightChecks = (int) Math.ceil(height - 1);
-                            for (int i = 0; i < heightChecks; i++)
-                            {
-                                if (!spawnPoint.add(0, i, 0).isAirBlock(world))
-                                {
-                                    return spawnAttempts >= spawnAttemptLimit;
-                                }
-                            }
-                        }
-                        //Set data
-                        entity.setPosition(spawnPoint.xi() + 0.5, spawnPoint.yi() + 0.5, spawnPoint.zi() + 0.5);
-                        //TODO rotate to face closest player
-
-                        //Spawn
-                        world.spawnEntityInWorld(entity);
-                        return true; //done
-                    }
-
-                    return spawnAttempts >= spawnAttemptLimit;
-                }
-            }
-
-            return true; //done due to bad data
         }
     }
 }
