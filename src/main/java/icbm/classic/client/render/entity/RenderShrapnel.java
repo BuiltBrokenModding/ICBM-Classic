a7af3f6888df9dfe23785c049ffce27d396168e5
Robert WC Seifert <rseifert.phone@gmail.com>
2018-01-19 02:44:05 -0500

    Implemented fragment render

--- src/main/java/icbm/classic/client/render/entity/RenderShrapnel.java
+++ src/main/java/icbm/classic/client/render/entity/RenderShrapnel.java
@@ -2,9 +2,14 @@ package icbm.classic.client.render.entity;
 
 import icbm.classic.ICBMClassic;
 import icbm.classic.content.entity.EntityFragments;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -15,7 +20,7 @@ public class RenderShrapnel extends Render<EntityFragments>
 {
     public static final ResourceLocation TEXTURE_FILE = new ResourceLocation(ICBMClassic.DOMAIN, "textures/models/fragment.png");
 
-    protected RenderShrapnel(RenderManager renderManager)
+    public RenderShrapnel(RenderManager renderManager)
     {
         super(renderManager);
     }
@@ -23,15 +28,70 @@ public class RenderShrapnel extends Render<EntityFragments>
     @Override
     public void doRender(EntityFragments entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
-        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+        this.bindEntityTexture(entity);
         if (entity.isAnvil)
         {
             //TODO render anvil block
         }
         else
         {
-            //TODO render shrapnel like arrows
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.pushMatrix();
+            GlStateManager.disableLighting();
+            GlStateManager.translate((float)x, (float)y, (float)z);
+            GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.0F, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks, 0.0F, 0.0F, 1.0F);
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder bufferbuilder = tessellator.getBuffer();
+
+            GlStateManager.enableRescaleNormal();
+            float f9 = (float)entity.arrowShake - partialTicks;
+
+            if (f9 > 0.0F)
+            {
+                float f10 = -MathHelper.sin(f9 * 3.0F) * f9;
+                GlStateManager.rotate(f10, 0.0F, 0.0F, 1.0F);
+            }
+
+            GlStateManager.rotate(45.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.scale(0.05625F, 0.05625F, 0.05625F);
+            GlStateManager.translate(-4.0F, 0.0F, 0.0F);
+
+            GlStateManager.glNormal3f(0.05625F, 0.0F, 0.0F);
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+            bufferbuilder.pos(-7.0D, -2.0D, -2.0D).tex(0.0D, 0.15625D).endVertex();
+            bufferbuilder.pos(-7.0D, -2.0D, 2.0D).tex(0.15625D, 0.15625D).endVertex();
+            bufferbuilder.pos(-7.0D, 2.0D, 2.0D).tex(0.15625D, 0.3125D).endVertex();
+            bufferbuilder.pos(-7.0D, 2.0D, -2.0D).tex(0.0D, 0.3125D).endVertex();
+            tessellator.draw();
+
+            GlStateManager.glNormal3f(-0.05625F, 0.0F, 0.0F);
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+            bufferbuilder.pos(-7.0D, 2.0D, -2.0D).tex(0.0D, 0.15625D).endVertex();
+            bufferbuilder.pos(-7.0D, 2.0D, 2.0D).tex(0.15625D, 0.15625D).endVertex();
+            bufferbuilder.pos(-7.0D, -2.0D, 2.0D).tex(0.15625D, 0.3125D).endVertex();
+            bufferbuilder.pos(-7.0D, -2.0D, -2.0D).tex(0.0D, 0.3125D).endVertex();
+            tessellator.draw();
+
+            for (int j = 0; j < 4; ++j)
+            {
+                GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+                GlStateManager.glNormal3f(0.0F, 0.0F, 0.05625F);
+                bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+                bufferbuilder.pos(-8.0D, -2.0D, 0.0D).tex(0.0D, 0.0D).endVertex();
+                bufferbuilder.pos(8.0D, -2.0D, 0.0D).tex(0.5D, 0.0D).endVertex();
+                bufferbuilder.pos(8.0D, 2.0D, 0.0D).tex(0.5D, 0.15625D).endVertex();
+                bufferbuilder.pos(-8.0D, 2.0D, 0.0D).tex(0.0D, 0.15625D).endVertex();
+                tessellator.draw();
+            }
+
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.enableLighting();
+            GlStateManager.popMatrix();
         }
+
+
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
     }
 
     @Nullable
