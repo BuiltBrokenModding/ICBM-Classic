54f6a2f2e069bac3ac76b72f70dedd4ddd008a0b
Robert WC Seifert <rseifert.phone@gmail.com>
2018-03-20 03:37:20 -0400

    Added recipe check if ore dictionary value exists
    Not sure why forge doesn't have this already

---
+++ src/main/java/icbm/classic/lib/recipe/ConditionOreValue.java
@@ -0,0 +1,40 @@
+package icbm.classic.lib.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.JsonUtils;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.function.BooleanSupplier;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 3/20/2018.
+ */
+public class ConditionOreValue implements IConditionFactory
+{
+    @Override
+    public BooleanSupplier parse(JsonContext context, JsonObject json)
+    {
+        final boolean condition = Boolean.parseBoolean(JsonUtils.getString(json, "condition").toLowerCase());
+        final String value = JsonUtils.getString(json, "value");
+        return () -> hasOreValue(value, condition);
+    }
+
+    private boolean hasOreValue(String value, boolean check)
+    {
+        NonNullList<ItemStack> list = OreDictionary.getOres(value);
+        boolean hasValue = false;
+        for (ItemStack stack : list)
+        {
+            if (!stack.isEmpty())
+            {
+                return hasValue == check;
+            }
+        }
+        return hasValue == check;
+    }
+}
