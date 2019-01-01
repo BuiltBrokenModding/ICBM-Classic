191ecb66d2fb73f69eca07598a80dbe0aa2cb7cc
Robert WC Seifert <rseifert.phone@gmail.com>
2019-01-01 18:22:26 -0500

    Disabled Debug

--- src/main/java/icbm/classic/content/entity/mobs/EntityXmasMob.java
+++ src/main/java/icbm/classic/content/entity/mobs/EntityXmasMob.java
@@ -178,7 +178,7 @@ public abstract class EntityXmasMob extends EntityMob implements IRangedAttackMo
     public void onUpdate()
     {
         super.onUpdate();
-        debugProjectileSpawns();
+        //debugProjectileSpawns();
     }
 
     protected void debugProjectileSpawns()
