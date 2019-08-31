9a1fc1be7b460d75ba9c8c230b0654502ec99a65
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-08-31 22:08:43 +0200

    improve render of explosive block's entities

--- src/main/java/icbm/classic/client/render/entity/RenderExBlock.java
+++ src/main/java/icbm/classic/client/render/entity/RenderExBlock.java
@@ -13,6 +13,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
@@ -47,12 +48,40 @@ public class RenderExBlock extends Render<EntityExplosive>
         GlStateManager.pushMatrix();
         GlStateManager.translate((float) x, (float) y + 0.5F, (float) z);
 
+        //expansion shortly before explosion
+        if ((float)entity.fuse - partialTicks + 1.0F < 10.0F)
+        {
+            float f = 1.0F - ((float)entity.fuse - partialTicks + 1.0F) / 10.0F;
+            f = MathHelper.clamp(f, 0.0F, 1.0F);
+            float f1 = 1.0F + f * f * f * 0.3F;
+            GlStateManager.scale(f1, f1, f1);
+        }
+
         this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 
         GlStateManager.rotate(-90.0F, 0.0F, 1.0F, 0.0F);
         GlStateManager.translate(-0.5F, -0.5F, 0.5F);
         blockrendererdispatcher.renderBlockBrightness(blockState, entity.getBrightness());
         GlStateManager.translate(0.0F, 0.0F, 1.0F);
+
+        //white flashing
+        if (entity.fuse / 5 % 2 == 0)
+        {
+            GlStateManager.disableTexture2D();
+            GlStateManager.disableLighting();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.DST_ALPHA);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, (1.0F - ((float)entity.fuse - partialTicks + 1.0F) / 100.0F) * 0.8F);
+            GlStateManager.doPolygonOffset(-3.0F, -3.0F);
+            GlStateManager.enablePolygonOffset();
+            blockrendererdispatcher.renderBlockBrightness(BlockReg.blockExplosive.getDefaultState(), 1.0F);
+            GlStateManager.doPolygonOffset(0.0F, 0.0F);
+            GlStateManager.disablePolygonOffset();
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.disableBlend();
+            GlStateManager.enableLighting();
+            GlStateManager.enableTexture2D();
+        }
 
         GlStateManager.popMatrix();
     }
