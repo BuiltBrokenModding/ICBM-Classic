396185e9e32b34e82ba1a2d042a6103b8ab10c87
Robert WC Seifert <rseifert.phone@gmail.com>
2019-09-22 19:23:15 -0400

    Reworked antimatter to better handle queues and threads

--- src/main/java/icbm/classic/content/blast/threaded/BlastAntimatter.java
+++ src/main/java/icbm/classic/content/blast/threaded/BlastAntimatter.java
@@ -4,16 +4,21 @@ import icbm.classic.api.events.BlastCancelEvent;
 import icbm.classic.client.ICBMSounds;
 import icbm.classic.config.ConfigBlast;
 import icbm.classic.content.blast.BlastHelpers;
-import icbm.classic.content.entity.EntityExplosion;
 import icbm.classic.content.blast.BlastRedmatter;
-import net.minecraft.block.Block;
+import icbm.classic.content.entity.EntityExplosion;
+import icbm.classic.lib.transform.BlockEditHandler;
+import net.minecraft.block.BlockFalling;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.MinecraftForge;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class BlastAntimatter extends BlastThreaded
 {
@@ -42,77 +47,21 @@ public class BlastAntimatter extends BlastThreaded
     @Override
     public void destroyBlock(BlockPos blockPos)
     {
-        if(!ConfigBlast.ANTIMATTER_BLOCK_AND_ENT_DAMAGE_ON_REDMATTER)
-            return;
-
         final IBlockState blockState = world.getBlockState(blockPos);
         if (!blockState.getBlock().isAir(blockState, world, blockPos))
         {
-            if(blockState.getBlock().getBlockHardness(blockState, world, blockPos) < 0.0F && !ConfigBlast.ANTIMATTER_DESTROY_UNBREAKABLE_BLOCKS) //unbreakable
-                return;
-
-            final double deltaX = blockPos.getX()-location.x();
-            if(deltaX > getBlastRadius() - 5)
-                makeHoles = true;
-
-            //final double deltaZ = blockPos.getZ()-blockPos.getZ();
-            //final double dist = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ); // omit Y since its not as important as X,Z and saves some performance
-            if(makeHoles) {
-
-                //TODO change to not use sqrt for better performance
-                //TODO maybe do the random in the thread? (although rolling the random numbers only happens at the end so it doesn't matter that much)
-
-                if (world().rand.nextFloat() < .6)// * (this.getBlastRadius() - dist))
-                    world.setBlockState(blockPos, replaceState, 3);
-
-                return; // dont clean up water anymore from this point on
-            }
-            else
+            if (blockState.getBlockHardness(world, blockPos) >= 0 || ConfigBlast.ANTIMATTER_DESTROY_UNBREAKABLE_BLOCKS)
             {
                 world.setBlockState(blockPos, replaceState, antimatterDoBlockUpdates ? 3 : 2);
             }
-
-            if(antimatterDoBlockUpdates) {
-                // handle sand and gravel (check if the above block is sand or gravel and then destroy it and check the above
-                destroyFallingBlocksRecursively(blockPos.up());
-
-                // remove water ahead of the blast, to significantly reduce the water issues
-                BlastHelpers.loopInRadius(antimatterWaterCleanupRange, (x, y, z) -> {
-                    final BlockPos bp2 = new BlockPos(blockPos.getX() + x, blockPos.getY() + 1, blockPos.getZ() + z);
-                    final IBlockState bs2 = world.getBlockState(bp2);
-                    final Block b = bs2.getBlock();
-                    if (b == Blocks.WATER || b == Blocks.FLOWING_WATER) {
-                        world.setBlockToAir(bp2);
-                    }
-                });
-            }
-        }
-    }
-
-    /*
-     *  Checks the current block and if it is sand or gravel then destroys it.
-     *  Continues upwards as long as there is sand or gravel.
-     */
-    private void destroyFallingBlocksRecursively(BlockPos currentBp)
-    {
-        Block currentBlock = world.getBlockState(currentBp).getBlock();
-        if (currentBlock == Blocks.SAND || currentBlock == Blocks.GRAVEL)
-        {
-            BlockPos above = currentBp.up();
-            destroyFallingBlocksRecursively(above); // check above block
-
-            world.setBlockState(currentBp, replaceState, 3); // destroy current
         }
     }
 
     @Override
     public boolean doRun(int loops, Consumer<BlockPos> edits)
     {
-        int ymin = -this.getPos().getY();
-        int ymax = 255-this.getPos().getY();
-        BlastHelpers.loopInRadius(this.getBlastRadius(), (x, y, z) ->{
-
-            if (y >= ymin && y < ymax)
+        BlastHelpers.loopInRadius(this.getBlastRadius(), (x, y, z) -> {
+            if (isInsideMap(y + yi()) && shouldEditPos(x, y, z))
             {
                 edits.accept(new BlockPos(xi() + x, yi() + y, zi() + z));
             }
@@ -121,22 +70,55 @@ public class BlastAntimatter extends BlastThreaded
     }
 
     @Override
-    public boolean doExplode(int callCount)
+    protected void onWorkerThreadComplete(List<BlockPos> edits)
     {
-        super.doExplode(callCount);
+        if (world instanceof WorldServer)
+        {
+            //Sort distance
+            edits.sort(buildSorter());
 
-        // TODO: Render antimatter shockwave
-        /*
-         * else if (ZhuYao.proxy.isGaoQing()) { for (int x = -this.getRadius(); x <
-         * this.getRadius(); x++) { for (int y = -this.getRadius(); y < this.getRadius(); y++) { for
-         * (int z = -this.getRadius(); z < this.getRadius(); z++) { Vector3 targetPosition =
-         * Vector3.add(position, new Vector3(x, y, z)); double distance =
-         * position.distanceTo(targetPosition);
-         * if (targetPosition.getBlockID(worldObj) == 0) { if (distance < this.getRadius() &&
-         * distance > this.getRadius() - 1 && worldObj.rand.nextFloat() > 0.5) {
-         * ParticleSpawner.spawnParticle("antimatter", worldObj, targetPosition); } } } } } }
-         */
-        return false;
+            //Pull out fluids and falling blocks to prevent lag issues
+            List<BlockPos> removeFirst = edits.stream().filter(blockPos -> {
+                IBlockState state = world.getBlockState(blockPos);
+                return state.getMaterial() == Material.WATER || state.getBlock() instanceof BlockFalling;
+            }).collect(Collectors.toList());
+
+            //Schedule edits to run in the world
+            ((WorldServer) world).addScheduledTask(() -> {
+
+                //Remove any blocks that could cause issues when queued
+                removeFirst.forEach(blockPos -> world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), 2));
+
+                //Queue edits, even the ones from the previous
+                BlockEditHandler.queue(world, edits, blockPos -> destroyBlock(blockPos));
+
+                //Notify blast we have entered world again
+                onPostThreadJoinWorld();
+            });
+        }
+    }
+
+    protected boolean isInsideMap(int y)
+    {
+        return y >= 0 && y < 256;
+    }
+
+    protected boolean shouldEditPos(int x, int y, int z)
+    {
+        final double distSQ = x * x + y * y + z * z;
+        final double blastSQ = this.getBlastRadius() * this.getBlastRadius();
+
+        final int featherEdge = (int)Math.floor(blastSQ * 0.05f);
+        final int delta = (int)Math.floor(blastSQ - distSQ);
+
+        if(delta < featherEdge)
+        {
+            final double p2 = 1 - (delta / (double)featherEdge);
+
+            System.out.println(featherEdge + " " + delta + " " + p2);
+            return world().rand.nextFloat() < p2;
+        }
+        return true;
     }
 
     @Override
@@ -153,8 +135,10 @@ public class BlastAntimatter extends BlastThreaded
         {
             if (((EntityExplosion) entity).getBlast() instanceof BlastRedmatter)
             {
-                if(!MinecraftForge.EVENT_BUS.post(new BlastCancelEvent(this, ((EntityExplosion) entity).getBlast())))
+                if (!MinecraftForge.EVENT_BUS.post(new BlastCancelEvent(this, ((EntityExplosion) entity).getBlast())))
+                {
                     entity.setDead();
+                }
                 return true;
             }
         }
