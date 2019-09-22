d5a0873952a4e4834fa9e5f8ded059bc4f6b14ca
Robert WC Seifert <rseifert.phone@gmail.com>
2019-09-22 19:22:07 -0400

    Started threading anti-gravity but its not done

--- src/main/java/icbm/classic/content/blast/BlastAntiGravitational.java
+++ src/main/java/icbm/classic/content/blast/BlastAntiGravitational.java
@@ -2,17 +2,23 @@ package icbm.classic.content.blast;
 
 import icbm.classic.ICBMClassic;
 import icbm.classic.api.explosion.IBlastTickable;
-import icbm.classic.content.entity.EntityFlyingBlock;
 import icbm.classic.content.blast.thread.ThreadSmallExplosion;
+import icbm.classic.content.blast.threaded.BlastThreaded;
+import icbm.classic.content.entity.EntityFlyingBlock;
 import icbm.classic.lib.transform.PosDistanceSorter;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
 
-public class BlastAntiGravitational extends Blast implements IBlastTickable
+public class BlastAntiGravitational extends BlastThreaded implements IBlastTickable
 {
     protected ThreadSmallExplosion thread;
     protected Set<EntityFlyingBlock> flyingBlocks = new HashSet<EntityFlyingBlock>();
@@ -30,11 +36,26 @@ public class BlastAntiGravitational extends Blast implements IBlastTickable
     }
 
     @Override
+    public boolean doRun(int loops, Consumer<BlockPos> edits)
+    {
+        int ymin = -this.getPos().getY();
+        int ymax = 255 -this.getPos().getY();
+        BlastHelpers.loopInRadius(this.getBlastRadius(), (x, y, z) ->{
+
+            if (y >= ymin && y < ymax)
+            {
+                edits.accept(new BlockPos(xi() + x, yi() + y, zi() + z));
+            }
+        });
+        return false;
+    }
+
+    @Override
     public boolean doExplode(int callCount) //TODO rewrite entire method
     {
         int r = this.callCount;
 
-        if(world() != null && !this.world().isRemote)
+        if (world() != null && !this.world().isRemote)
         {
             try
             {
@@ -91,10 +112,10 @@ public class BlastAntiGravitational extends Blast implements IBlastTickable
                 else
                 {
                     String msg = String.format("BlastAntiGravitational#doPostExplode() -> Failed to run due to null thread" +
-                            "\nWorld = %s " +
-                            "\nThread = %s" +
-                            "\nSize = %s" +
-                            "\nPos = ",
+                                    "\nWorld = %s " +
+                                    "\nThread = %s" +
+                                    "\nSize = %s" +
+                                    "\nPos = ",
                             world, thread, size, location);
                     ICBMClassic.logger().error(msg);
                 }
@@ -102,10 +123,10 @@ public class BlastAntiGravitational extends Blast implements IBlastTickable
             catch (Exception e)
             {
                 String msg = String.format("BlastAntiGravitational#doPostExplode() ->  Unexpected error while running post detonation code " +
-                        "\nWorld = %s " +
-                        "\nThread = %s" +
-                        "\nSize = %s" +
-                        "\nPos = ",
+                                "\nWorld = %s " +
+                                "\nThread = %s" +
+                                "\nSize = %s" +
+                                "\nPos = ",
                         world, thread, size, location);
                 ICBMClassic.logger().error(msg, e);
             }
@@ -132,16 +153,6 @@ public class BlastAntiGravitational extends Blast implements IBlastTickable
     @Override
     protected void onBlastCompleted()
     {
-        for (EntityFlyingBlock entity : flyingBlocks)
-        {
-            entity.gravity = 0.045f;
-        }
+        flyingBlocks.forEach(EntityFlyingBlock::restoreGravity);
     }
-
-    @Override
-    public float getBlastRadius()
-    {
-        return 15;
-    }
-
 }
