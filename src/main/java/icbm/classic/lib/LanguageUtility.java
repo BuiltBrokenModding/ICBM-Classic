f98b89dd6b49e0860647b30eacc45bfcc4eba5e3
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-29 19:00:33 +0100

    no, scala! bad scala!

--- src/main/java/icbm/classic/lib/LanguageUtility.java
+++ src/main/java/icbm/classic/lib/LanguageUtility.java
@@ -5,9 +5,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.translation.I18n;
-import scala.actors.threadpool.Arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
