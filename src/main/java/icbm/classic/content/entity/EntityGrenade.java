0e0eed13fdeb44077e0e60bac263921858ca26ee
Robert WC Seifert <rseifert.phone@gmail.com>
2020-03-28 21:54:58 -0400

    4.0.1

--- src/main/java/icbm/classic/content/entity/EntityGrenade.java
+++ src/main/java/icbm/classic/content/entity/EntityGrenade.java
@@ -70,7 +70,7 @@ public class EntityGrenade extends Entity implements IEntityAdditionalSpawnData
      *
      * @return this
      */
-    public EntityGrenade aimFromThrower()
+    public EntityGrenade aimFromThrower() //TODO figure out which hand threw the grenade so we can spawn over shoulder
     {
         this.setLocationAndAngles(thrower.posX, thrower.posY + thrower.getEyeHeight(), thrower.posZ, thrower.rotationYaw, thrower.rotationPitch);
 
