32bc2385fc7536aa9e1f115300c25e6acdcca218
GHXX <GHXX@gmx.at>
2020-04-14 03:48:51 +0200

    dummy commit to retrigger ci

--- src/test/java/icbm/classic/config/ConfigFieldTests.java
+++ src/test/java/icbm/classic/config/ConfigFieldTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
