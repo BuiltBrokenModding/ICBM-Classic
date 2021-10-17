809f47a083b8d7c8ce380b34e954a72baaad795a
Robin Seifert <rseifert.phone@gmail.com>
2021-10-17 00:32:37 -0400

    Added redmatter damage source and position logic

--- src/main/java/icbm/classic/content/blast/redmatter/DamageSourceRedmatter.java
+++ src/main/java/icbm/classic/content/blast/redmatter/DamageSourceRedmatter.java
@@ -1,7 +1,9 @@
 package icbm.classic.content.blast.redmatter;
 
 import icbm.classic.content.blast.redmatter.logic.RedmatterLogic;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.Vec3d;
 
 /**
  * Created by Dark(DarkGuardsman, Robert) on 5/23/2020.
@@ -14,9 +16,24 @@ public class DamageSourceRedmatter extends DamageSource
     {
         super("icbm.redmatter");
         this.blastRedmatter = blastRedmatter;
+        setDamageBypassesArmor();
+        setDamageIsAbsolute();
+
     }
 
-    //TODO add handling for source
+    @Override
+    public Entity getTrueSource()
+    {
+        return blastRedmatter != null ? blastRedmatter.host : null; //TODO see if we can get trigger source
+    }
+
+    @Override
+    public Vec3d getDamageLocation()
+    {
+        return blastRedmatter != null && blastRedmatter.host != null
+                ? blastRedmatter.host.getPositionVector()
+                : null;
+    }
 
     //TODO add handling for death messages
 }
