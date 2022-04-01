c0681567da917254a706df224d67924c6e16fedb
Robin Seifert <rseifert.phone@gmail.com>
2022-04-01 05:58:40 -0400

    refactor: split HSB color logic from redmatter render

---
+++ src/main/java/icbm/classic/client/ColorHelper.java
@@ -0,0 +1,60 @@
+package icbm.classic.client;
+
+import org.lwjgl.util.Color;
+
+/**
+ * Created by Robin Seifert on 4/1/2022.
+ */
+public final class ColorHelper
+{
+    private ColorHelper() {}
+
+    /**
+     * From java.awt.Color... copied and modified to avoid loading awt module
+     */
+    public static Color HSBtoRGB(float hue, float saturation, float brightness) {
+        int r = 0, g = 0, b = 0;
+        if (saturation == 0) {
+            r = g = b = (int) (brightness * 255.0f + 0.5f);
+        } else {
+            float h = (hue - (float)Math.floor(hue)) * 6.0f;
+            float f = h - (float)java.lang.Math.floor(h);
+            float p = brightness * (1.0f - saturation);
+            float q = brightness * (1.0f - saturation * f);
+            float t = brightness * (1.0f - (saturation * (1.0f - f)));
+            switch ((int) h) {
+                case 0:
+                    r = (int) (brightness * 255.0f + 0.5f);
+                    g = (int) (t * 255.0f + 0.5f);
+                    b = (int) (p * 255.0f + 0.5f);
+                    break;
+                case 1:
+                    r = (int) (q * 255.0f + 0.5f);
+                    g = (int) (brightness * 255.0f + 0.5f);
+                    b = (int) (p * 255.0f + 0.5f);
+                    break;
+                case 2:
+                    r = (int) (p * 255.0f + 0.5f);
+                    g = (int) (brightness * 255.0f + 0.5f);
+                    b = (int) (t * 255.0f + 0.5f);
+                    break;
+                case 3:
+                    r = (int) (p * 255.0f + 0.5f);
+                    g = (int) (q * 255.0f + 0.5f);
+                    b = (int) (brightness * 255.0f + 0.5f);
+                    break;
+                case 4:
+                    r = (int) (t * 255.0f + 0.5f);
+                    g = (int) (p * 255.0f + 0.5f);
+                    b = (int) (brightness * 255.0f + 0.5f);
+                    break;
+                case 5:
+                    r = (int) (brightness * 255.0f + 0.5f);
+                    g = (int) (p * 255.0f + 0.5f);
+                    b = (int) (q * 255.0f + 0.5f);
+                    break;
+            }
+        }
+        return new Color(r, g, b);
+    }
+}
--- src/main/java/icbm/classic/content/blast/redmatter/render/RenderRedmatter.java
+++ src/main/java/icbm/classic/content/blast/redmatter/render/RenderRedmatter.java
@@ -1,6 +1,7 @@
 package icbm.classic.content.blast.redmatter.render;
 
 import icbm.classic.ICBMConstants;
+import icbm.classic.client.ColorHelper;
 import icbm.classic.config.blast.ConfigBlast;
 import icbm.classic.content.blast.redmatter.EntityRedmatter;
 import net.minecraft.client.renderer.BufferBuilder;
@@ -246,7 +247,7 @@ public class RenderRedmatter extends Render<EntityRedmatter>
                     final float hue = redmatterBeamRandom.nextFloat();
                     final float saturation = 0.9f;//1.0 for brilliant, 0.0 for dull
                     final float luminance = 0.5f; //1.0 for brighter, 0.0 for black
-                    colorOut = HSBtoRGB(hue, saturation, luminance);
+                    colorOut = ColorHelper.HSBtoRGB(hue, saturation, luminance);
 
                     randomColorsForBeams.add(colorOut);
                 }
@@ -299,54 +300,5 @@ public class RenderRedmatter extends Render<EntityRedmatter>
     protected ResourceLocation getEntityTexture(EntityRedmatter entity)
     {
         return GREY_TEXTURE;
-    }
-
-    /**
-     * From java.awt.Color... copied and modified to avoid loading awt module
-     */
-    public static Color HSBtoRGB(float hue, float saturation, float brightness) {
-        int r = 0, g = 0, b = 0;
-        if (saturation == 0) {
-            r = g = b = (int) (brightness * 255.0f + 0.5f);
-        } else {
-            float h = (hue - (float)Math.floor(hue)) * 6.0f;
-            float f = h - (float)java.lang.Math.floor(h);
-            float p = brightness * (1.0f - saturation);
-            float q = brightness * (1.0f - saturation * f);
-            float t = brightness * (1.0f - (saturation * (1.0f - f)));
-            switch ((int) h) {
-                case 0:
-                    r = (int) (brightness * 255.0f + 0.5f);
-                    g = (int) (t * 255.0f + 0.5f);
-                    b = (int) (p * 255.0f + 0.5f);
-                    break;
-                case 1:
-                    r = (int) (q * 255.0f + 0.5f);
-                    g = (int) (brightness * 255.0f + 0.5f);
-                    b = (int) (p * 255.0f + 0.5f);
-                    break;
-                case 2:
-                    r = (int) (p * 255.0f + 0.5f);
-                    g = (int) (brightness * 255.0f + 0.5f);
-                    b = (int) (t * 255.0f + 0.5f);
-                    break;
-                case 3:
-                    r = (int) (p * 255.0f + 0.5f);
-                    g = (int) (q * 255.0f + 0.5f);
-                    b = (int) (brightness * 255.0f + 0.5f);
-                    break;
-                case 4:
-                    r = (int) (t * 255.0f + 0.5f);
-                    g = (int) (p * 255.0f + 0.5f);
-                    b = (int) (brightness * 255.0f + 0.5f);
-                    break;
-                case 5:
-                    r = (int) (brightness * 255.0f + 0.5f);
-                    g = (int) (p * 255.0f + 0.5f);
-                    b = (int) (q * 255.0f + 0.5f);
-                    break;
-            }
-        }
-        return new Color(r, g, b);
     }
 }
