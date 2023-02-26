282a3dc2c45e1069e0b37e5325ad02aae8d8a1d4
Robin Seifert <rseifert.phone@gmail.com>
2023-02-26 02:42:17 -0500

    chore: updated launch position

--- src/main/java/icbm/classic/content/blocks/launcher/base/TileLauncherBase.java
+++ src/main/java/icbm/classic/content/blocks/launcher/base/TileLauncherBase.java
@@ -198,7 +198,7 @@ public class TileLauncherBase extends TilePoweredMachine
                 {
                     final IMissile missile = missileStack.newMissile(world());
                     final Entity entity = missile.getMissileEntity();
-                    entity.setPosition(xi() + 0.5, yi() + 2.2, zi() + 0.5);  //TODO store offset as variable, sync with missile height
+                    entity.setPosition(xi() + 0.5, yi() + 3.1, zi() + 0.5);  //TODO store offset as variable, sync with missile height
 
                     //Trigger launch event
                     missile.setTargetData(new BallisticTargetingData(target, 1));
