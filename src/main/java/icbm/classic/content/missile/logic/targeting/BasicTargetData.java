f304cbf8589b7ab66ea748dd179457cdb6992f1b
Robin Seifert <rseifert.phone@gmail.com>
2023-04-18 09:19:52 -0400

    chore: implemented firing delay into basic target data

--- src/main/java/icbm/classic/content/missile/logic/targeting/BasicTargetData.java
+++ src/main/java/icbm/classic/content/missile/logic/targeting/BasicTargetData.java
@@ -2,6 +2,7 @@ package icbm.classic.content.missile.logic.targeting;
 
 import icbm.classic.ICBMConstants;
 import icbm.classic.api.missiles.parts.IMissileTarget;
+import icbm.classic.api.missiles.parts.IMissileTargetDelayed;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -9,10 +10,14 @@ import net.minecraft.util.math.Vec3d;
 
 import java.util.Objects;
 
-public class BasicTargetData implements IMissileTarget {
+/**
+ * Simple 3D position targeting data
+ */
+public class BasicTargetData implements IMissileTarget, IMissileTargetDelayed {
 
     public static final ResourceLocation REG_NAME = new ResourceLocation(ICBMConstants.DOMAIN, "basic");
     private Vec3d position;
+    private int firingDelay = 0;
 
     public BasicTargetData() {
         //Only used for save/load
@@ -32,6 +37,16 @@ public class BasicTargetData implements IMissileTarget {
 
     public Vec3d getPosition() {
         return position;
+    }
+
+    public BasicTargetData withFiringDelay(int ticks) {
+        this.firingDelay = ticks;
+        return this;
+    }
+
+    @Override
+    public int getFiringDelay() {
+        return firingDelay;
     }
 
     @Override
@@ -69,6 +84,7 @@ public class BasicTargetData implements IMissileTarget {
         saveData.setDouble("x", position.x);
         saveData.setDouble("y", position.y);
         saveData.setDouble("z", position.z);
+        saveData.setInteger("firingDelay", firingDelay);
         return saveData;
     }
 
@@ -78,6 +94,7 @@ public class BasicTargetData implements IMissileTarget {
         double y = nbt.getDouble("y");
         double z = nbt.getDouble("z");
         this.position = new Vec3d(x, y, z);
+        this.firingDelay = nbt.getInteger("firingDelay");
     }
 
     @Override
