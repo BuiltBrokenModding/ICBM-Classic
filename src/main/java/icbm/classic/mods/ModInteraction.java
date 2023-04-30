f9e6a344185e852df3733cb826e832b49fb2e26b
Robin Seifert <rseifert.phone@gmail.com>
2023-04-29 20:19:55 -0400

    feat: moving cc support to addon project

--- src/main/java/icbm/classic/mods/ModInteraction.java
+++ src/main/java/icbm/classic/mods/ModInteraction.java
@@ -1,6 +1,5 @@
 package icbm.classic.mods;
 
-import icbm.classic.mods.cc.CCProxy;
 import icbm.classic.mods.ic2.IC2Proxy;
 import icbm.classic.mods.mekanism.MekProxy;
 
@@ -9,18 +8,15 @@ public class ModInteraction {
     public static void preInit() {
         IC2Proxy.INSTANCE.preInit();
         MekProxy.INSTANCE.preInit();
-        CCProxy.INSTANCE.preInit();
     }
 
     public static void init() {
         IC2Proxy.INSTANCE.init();
         MekProxy.INSTANCE.init();
-        CCProxy.INSTANCE.init();
     }
 
     public static void postInit() {
         IC2Proxy.INSTANCE.postInit();
         MekProxy.INSTANCE.postInit();
-        CCProxy.INSTANCE.postInit();
     }
 }
