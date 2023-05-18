3ad3a8820747042a558fbd72275005f2f52fae60
Robin Seifert <rseifert.phone@gmail.com>
2023-05-18 15:22:00 -0400

    feat: dropping ic2 support

--- src/main/java/icbm/classic/mods/ModInteraction.java
+++ src/main/java/icbm/classic/mods/ModInteraction.java
@@ -6,17 +6,14 @@ import icbm.classic.mods.mekanism.MekProxy;
 public class ModInteraction {
 
     public static void preInit() {
-        IC2Proxy.INSTANCE.preInit();
         MekProxy.INSTANCE.preInit();
     }
 
     public static void init() {
-        IC2Proxy.INSTANCE.init();
         MekProxy.INSTANCE.init();
     }
 
     public static void postInit() {
-        IC2Proxy.INSTANCE.postInit();
         MekProxy.INSTANCE.postInit();
     }
 }
