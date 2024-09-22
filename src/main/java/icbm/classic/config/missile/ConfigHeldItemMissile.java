92b38feb8b5ca301aa9bf93b855442da02a1b381
Robin Seifert <rseifert.phone@gmail.com>
2024-09-22 15:02:32 -0400

    feat: config for missile block break multiplier

--- src/main/java/icbm/classic/config/missile/ConfigHeldItemMissile.java
+++ src/main/java/icbm/classic/config/missile/ConfigHeldItemMissile.java
@@ -7,13 +7,17 @@ import net.minecraftforge.common.config.Config;
  */
 public class ConfigHeldItemMissile
 {
-    @Config.Name("item_ban_allow")
+    @Config.Name("item_ban_allow") // TODO rename to `items`
     @Config.Comment("Items allowed for insertion into held-item missile as a projectile payload")
     public BanList BAN_ALLOW = new BanList();
 
+    @Config.Name("block_damage_multiplier")
+    @Config.Comment("Multiplier to apply to missile's held item's block break speed.")
+    public float BLOCK_DAMAGE_MULTIPLIER = 4;
+
     public static class BanList {
 
-        @Config.Name("ban")
+        @Config.Name("ban") //TODO rename to `invert`
         @Config.Comment("Set to true to ban all blocks contained. False to use as allow list")
         public boolean BAN = true;
 
