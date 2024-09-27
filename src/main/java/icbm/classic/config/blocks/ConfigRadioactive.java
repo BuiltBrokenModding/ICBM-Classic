64df178146e7bf4069c5a8af827fc94b15d0a794
Robin Seifert <rseifert.phone@gmail.com>
2024-09-27 14:45:10 -0400

    feat: chance for radioactive blocks to decay to non-radioactive version

--- src/main/java/icbm/classic/config/blocks/ConfigRadioactive.java
+++ src/main/java/icbm/classic/config/blocks/ConfigRadioactive.java
@@ -4,23 +4,29 @@ import net.minecraftforge.common.config.Config;
 
 public class ConfigRadioactive {
 
-    @Config.Name("decay_chance")
-    @Config.Comment("Chance for radioactive decay effects to be triggered")
+    @Config.Name("decay_delay")
+    @Config.Comment("Delay in ticks (20 ticks per second) between running decay chance calculation and effects. Set to zero to disable, if previously zero requires world restart for old blocks to tick.")
+    @Config.RangeInt(min = 1)
+    public int decayDelay = 5;
+
+    @Config.Name("decay_block_chance")
+    @Config.Comment("Chance (random < value) for radioactive block to convert into harmless blocks, EX: Radioactive Stone -> Stone")
     @Config.RangeDouble(min = 0, max = 1)
-    public float decayChance = 0.85f;
+    public float decayBlockChance = 0.01f;
+
+    @Config.Name("decay_effect_chance")
+    @Config.Comment("Chance (random < value) for radioactive decay effects to be triggered per entity")
+    @Config.RangeDouble(min = 0, max = 1)
+    public float decayEffectChance = 0.85f;
 
     @Config.Name("decay_effect_range")
     @Config.Comment("Range (meters) for effects to be applied")
     @Config.RangeInt(min = 1)
-    public int decayRange = 5;
+    public int decayEffectRange = 5;
 
-    @Config.Name("decay_delay")
-    @Config.Comment("Delay (20 ticks per second) between running decay chance calculation and effects. Set to zero to disable, if previously zero requires world restart for old blocks to tick.")
-    @Config.RangeInt(min = 1)
-    public int decayDelay = 5;
 
-    @Config.Name("decay_damage")
+    @Config.Name("decay_effect_damage")
     @Config.Comment("Damage to apply to entities caught in decay range")
     @Config.RangeDouble(min = 1)
-    public float decayDamage = 2;
+    public float decayEffectDamage = 2;
 }
