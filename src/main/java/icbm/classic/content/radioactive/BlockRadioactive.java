d94808a85c7dea8953de1346419d589ae20db334
Robin Seifert <rseifert.phone@gmail.com>
2024-09-28 16:44:08 -0400

    refactor: simplifying random to not need max check

--- src/main/java/icbm/classic/content/radioactive/BlockRadioactive.java
+++ src/main/java/icbm/classic/content/radioactive/BlockRadioactive.java
@@ -66,7 +66,7 @@ public class BlockRadioactive extends Block {
             this.tryDecayEntities(worldIn, pos, state, random);
 
             if(!ConfigBlocks.radioactive.randomTickOnly) {
-                worldIn.scheduleUpdate(pos, this, Math.max(1, random.nextInt(tickRate(worldIn) + 1)));
+                worldIn.scheduleUpdate(pos, this, random.nextInt(tickRate(worldIn)) + 1);
             }
         }
     }
