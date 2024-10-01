1a04e0a150275e25a0756f76493c13fb4f43fec2
Robin Seifert <rseifert.phone@gmail.com>
2024-10-01 12:07:58 -0400

    fix: ore names being registered incorrectly

--- src/main/java/icbm/classic/content/reg/ItemReg.java
+++ src/main/java/icbm/classic/content/reg/ItemReg.java
@@ -19,6 +19,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.oredict.OreDictionary;
