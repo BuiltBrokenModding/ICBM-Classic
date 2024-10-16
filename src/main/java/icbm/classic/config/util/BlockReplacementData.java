0669f0206cf0fae1e90ac5b2d5945f8460b7ac5b
Robin Seifert <rseifert.phone@gmail.com>
2024-10-16 01:01:55 -0400

    feat: adding nbt support for radioactive block swaps

--- src/main/java/icbm/classic/config/util/BlockReplacementData.java
+++ src/main/java/icbm/classic/config/util/BlockReplacementData.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
  * Handles block placement with a chance
@@ -12,19 +15,47 @@ import net.minecraft.nbt.NBTTagCompound;
 @Accessors(chain = true)
 public class BlockReplacementData {
 
-
     /** Block state to place */
     private IBlockState blockState;
 
     /** NBT data to load into the block */
     private NBTTagCompound blockNBT = null;
 
-    /** Should the target block's nbt be cloned before reading */
-    private boolean copyTargetNBT = false;
-
-    /** Should the source block's nbt be cloned before reading */
-    private boolean copySourceNBT = false;
-
     /** Chance of placement, 1 is always and 0 is never */
     private float chance = -1;
+
+    public void apply(World world, BlockPos pos) {
+        if (shouldPlace(world)) {
+            placeBlock(world, pos);
+            applyNBT(world, pos);
+        }
+    }
+
+    private boolean shouldPlace(World world) {
+        return getChance() < 0 || world.rand.nextFloat() <= getChance();
+    }
+
+    private void placeBlock(World world, BlockPos pos) {
+        if (getBlockState() != null) {
+            world.setBlockState(pos, getBlockState(), 3);
+        }
+    }
+
+    private void applyNBT(World world, BlockPos pos) {
+        if(blockNBT != null && !blockNBT.hasNoTags()) {
+            final TileEntity tileEntity = world.getTileEntity(pos);
+            if(tileEntity != null) {
+                final NBTTagCompound existingSave = tileEntity.writeToNBT(new NBTTagCompound());
+
+                // Remove internal tags to prevent issues
+                blockNBT.removeTag("id");
+                blockNBT.removeTag("x");
+                blockNBT.removeTag("y");
+                blockNBT.removeTag("z");
+
+                existingSave.merge(blockNBT);
+                tileEntity.readFromNBT(existingSave);
+            }
+        }
+    }
 }
