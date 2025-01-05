cc9b87b99f660efa78aadf3805cabe3875274a70
Robin Seifert <rseifert.phone@gmail.com>
2025-01-05 02:56:44 -0500

    fix: buildable object save node not handling missing registry entry

--- src/main/java/icbm/classic/lib/saving/nodes/SaveBuildableObject.java
+++ src/main/java/icbm/classic/lib/saving/nodes/SaveBuildableObject.java
@@ -1,5 +1,7 @@
 package icbm.classic.lib.saving.nodes;
 
+import icbm.classic.ICBMClassic;
+import icbm.classic.api.missiles.projectile.IProjectileData;
 import icbm.classic.api.reg.obj.IBuildableObject;
 import icbm.classic.api.reg.obj.IBuilderRegistry;
 import icbm.classic.lib.saving.NbtSaveNode;
@@ -11,11 +13,25 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class SaveBuildableObject<E, C extends IBuildableObject> extends NbtSaveNode<E, CompoundNBT>  {
-    public SaveBuildableObject(String name, final Supplier<IBuilderRegistry<C>> reg, Function<E, C> getter, BiConsumer<E, C> setter) {
+    public SaveBuildableObject(String name, final Supplier<IBuilderRegistry<C>> builderRegistry, Function<E, C> getter, BiConsumer<E, C> setter) {
         super(name,
-            (source) -> Optional.ofNullable(getter.apply(source)).map(reg.get()::save).orElse(null),
+            (source) -> {
+                final C object = getter.apply(source);
+                if(object != null) {
+                    if(builderRegistry.get() == null){
+                        ICBMClassic.logger().error("Failed to save buildable object node, builder registry is null...field:{}",name);
+                        return null;
+                    }
+                    return builderRegistry.get().save(object);
+                }
+                return null;
+            },
             (source, data) -> {
-                final C object = reg.get().load(data);
+                if(builderRegistry.get() == null){
+                    ICBMClassic.logger().error("Failed to load buildable object node, builder registry is null...field:{}",name);
+                    return;
+                }
+                final C object = builderRegistry.get().load(data);
                 if(object != null) {
                     setter.accept(source, object);
                 }
