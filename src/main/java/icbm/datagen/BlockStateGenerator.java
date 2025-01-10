2462ea0ecb182b0177eaca4df50009d88773eedd
Robin Seifert <rseifert.phone@gmail.com>
2025-01-10 09:53:45 -0500

    refactor: switching concrete block state to data gen

--- src/main/java/icbm/datagen/BlockStateGenerator.java
+++ src/main/java/icbm/datagen/BlockStateGenerator.java
@@ -3,6 +3,7 @@ package icbm.datagen;
 import icbm.classic.content.reg.BlockReg;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -16,9 +17,13 @@ public class BlockStateGenerator extends BlockStateProvider {
     protected void registerStatesAndModels() {
         simpleModel(BlockReg.RADIOACTIVE_DIRT.get());
         simpleModel(BlockReg.RADIOACTIVE_STONE.get());
+
+        simpleModel(BlockReg.CONCRETE_NORMAL.get());
+        simpleModel(BlockReg.CONCRETE_COMPACT.get());
+        simpleModel(BlockReg.CONCRETE_REINFORCED.get());
     }
 
     private void simpleModel(Block block) {
-        simpleBlock(block, new ModelFile.UncheckedModelFile("block/" + block.getRegistryName().getPath()));
+        simpleBlock(block, new ModelFile.UncheckedModelFile(new ResourceLocation(block.getRegistryName().getNamespace(), "block/" + block.getRegistryName().getPath())));
     }
 }
