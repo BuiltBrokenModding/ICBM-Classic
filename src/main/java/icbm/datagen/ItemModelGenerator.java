8978ae927e9065426312617b79f69ecc422e2c84
Robin Seifert <rseifert.phone@gmail.com>
2025-01-11 09:10:01 -0500

    refactor: data gen for grenade item models

--- src/main/java/icbm/datagen/ItemModelGenerator.java
+++ src/main/java/icbm/datagen/ItemModelGenerator.java
@@ -1,6 +1,5 @@
 package icbm.datagen;
 
-import icbm.classic.ICBMClassic;
 import icbm.classic.ICBMConstants;
 import icbm.classic.content.reg.ItemReg;
 import net.minecraft.data.DataGenerator;
@@ -21,13 +20,28 @@ public class ItemModelGenerator extends ItemModelProvider {
         singleLayerItem(ItemReg.DUST_SULFUR);
         singleLayerItem(ItemReg.DUST_SALTPETER);
         singleLayerItem(ItemReg.CLUMP_SALTPETER);
-        
+
+        grenade(ItemReg.GRENADE_CONDENSED); //TODO consider gradle tool to flatten paths
+        grenade(ItemReg.GRENADE_SHRAPNEL);
+        grenade(ItemReg.GRENADE_INCENDIARY);
+        grenade(ItemReg.GRENADE_DEBILITATION);
+        grenade(ItemReg.GRENADE_CHEMICAL);
+        grenade(ItemReg.GRENADE_ANVIL);
+        grenade(ItemReg.GRENADE_REPULSIVE);
+        grenade(ItemReg.GRENADE_ATTRACTIVE);
+
+
         singleLayerItem(ItemReg.ANTIDOTE_PILL);
     }
 
     private void singleLayerItem(RegistryObject<? extends Item> item) {
         this.withExistingParent(name(item), "item/generated")
             .texture("layer0", texture(name(item)));
+    }
+
+    private void grenade(RegistryObject<? extends Item> item) {
+        this.withExistingParent(name(item), "icbm:item/grenade")
+            .texture("layer0", texture("grenade" + "/" + name(item).replace("grenade" + "_", "")));
     }
 
     private String name(RegistryObject<? extends Item> item) {
