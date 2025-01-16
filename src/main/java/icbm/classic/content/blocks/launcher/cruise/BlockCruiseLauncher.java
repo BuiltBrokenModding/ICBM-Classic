42ebf76feebbaceb4ce5a73b30e66ed471fcbdeb
Robin Seifert <rseifert.phone@gmail.com>
2025-01-15 23:50:35 -0500

    chore: voxel shape for cruise launcher

--- src/main/java/icbm/classic/content/blocks/launcher/cruise/BlockCruiseLauncher.java
+++ src/main/java/icbm/classic/content/blocks/launcher/cruise/BlockCruiseLauncher.java
@@ -8,16 +8,22 @@ import icbm.classic.lib.capability.gps.GPSDataHelpers;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.IHopper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
@@ -29,10 +35,20 @@ import javax.annotation.Nullable;
  */
 public class BlockCruiseLauncher extends Block
 {
+    private static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(
+        Block.makeCuboidShape(0, 0, 0, 16, 4, 16),
+        Block.makeCuboidShape(2, 4, 2, 14, 16, 14),
+        IBooleanFunction.OR
+    );
+
     public BlockCruiseLauncher(Properties properties)
     {
         super(properties);
-        //TODO set voxel shape
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
     }
 
     @Override
